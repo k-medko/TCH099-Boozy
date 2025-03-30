a2c4197bfff0aeff78e1d6f121a131b9dd61597a
Emix555 <emilesimoneau04@gmail.com>
2025-03-30 15:17:49 -0400

    Update create.sql
    jai mit les not null/null

--- DATABASE/create.sql
+++ DATABASE/create.sql
@@ -13,65 +13,65 @@ DROP TABLE IF EXISTS UserAccount;
 CREATE TABLE UserAccount (
     user_id          INT(10) PRIMARY KEY,
     email            VARCHAR(255) UNIQUE,
-    last_name        VARCHAR(255),
-    first_name       VARCHAR(255),
+    last_name        VARCHAR(255) NOT NULL,
+    first_name       VARCHAR(255) NOT NULL,
     phone_number     VARCHAR(15),
-    user_type        ENUM('customer', 'deliverer', 'admin'),
-    license_plate    VARCHAR(25)
+    user_type        ENUM('customer', 'deliverer', 'admin') NOT NULL,
+    license_plate    VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE CustomerAddress (
     address_id    INT(10) PRIMARY KEY,
-    house_number  INT(5),
-    street        VARCHAR(255),
-    postal_code   VARCHAR(25),
-    city          VARCHAR(255),
-    user_id       INT(10),
+    house_number  INT(5) NOT NULL,
+    street        VARCHAR(255) NOT NULL,
+    postal_code   VARCHAR(25) NOT NULL,
+    city          VARCHAR(255) NOT NULL,
+    user_id       INT(10) NOT NULL,
     CONSTRAINT fk_address_user_id FOREIGN KEY (user_id) REFERENCES UserAccount(user_id)
 );
 
 CREATE TABLE StoreLocation (
     store_id     INT(10) PRIMARY KEY,
-    name         VARCHAR(255),
-    address_id   INT(10),
+    name         VARCHAR(255) NOT NULL,
+    address_id   INT(10) NOT NULL,
     CONSTRAINT fk_store_address_id FOREIGN KEY (address_id) REFERENCES CustomerAddress(address_id)
 );
 
 CREATE TABLE Product (
     product_id     INT(10) PRIMARY KEY,
-    name           VARCHAR(255),
+    name           VARCHAR(255) NOT NULL,
     description    VARCHAR(1000),
-    price          DECIMAL(10,2),
-    category       VARCHAR(100),
-    is_available   BOOLEAN
+    price          DECIMAL(10,2) NOT NULL,
+    category       VARCHAR(100) NOT NULL,
+    is_available   BOOLEAN  NOT NULL
 );
 
 CREATE TABLE CustomerOrder (
     order_id        INT(10) PRIMARY KEY,
-    creation_date   DATETIME,
-    status          VARCHAR(15),
-    total_amount    DECIMAL(10,2),
-    address_id      INT(10),
-    store_id        INT(10),
-    user_id         INT(10),
+    creation_date   DATETIME NOT NULL,
+    status          VARCHAR(15) NOT NULL,
+    total_amount    DECIMAL(10,2) NOT NULL,
+    address_id      INT(10) NOT NULL,
+    store_id        INT(10) NOT NULL,
+    user_id         INT(10) NOT NULL,
     CONSTRAINT fk_customerorder_address_id FOREIGN KEY (address_id) REFERENCES CustomerAddress(address_id),
     CONSTRAINT fk_customerorder_store_id FOREIGN KEY (store_id) REFERENCES StoreLocation(store_id),
     CONSTRAINT fk_customerorder_user_id FOREIGN KEY (user_id) REFERENCES UserAccount(user_id)
 );
 
 CREATE TABLE OrderItems (
-    order_id      INT(10),
-    product_id    INT(10),
-    quantity      INT(5),
+    order_id      INT(10) NOT NULL,
+    product_id    INT(10) NOT NULL,
+    quantity      INT(5) NOT NULL,
     PRIMARY KEY (order_id, product_id),
     CONSTRAINT fk_orderitems_order_id FOREIGN KEY (order_id) REFERENCES CustomerOrder(order_id),
     CONSTRAINT fk_orderitems_product_id FOREIGN KEY (product_id) REFERENCES Product(product_id)
 );
 
 CREATE TABLE ProductInventory (
-    store_id       INT(10),
-    product_id     INT(10),
-    quantity       INT(5),
+    store_id       INT(10) NOT NULL,
+    product_id     INT(10) NOT NULL,
+    quantity       INT(5) NOT NULL,
     CONSTRAINT pk_productinventory_primary_keys PRIMARY KEY (store_id, product_id),
     CONSTRAINT fk_productinventory_store_id FOREIGN KEY (store_id) REFERENCES StoreLocation(store_id),
     CONSTRAINT fk_productinventory_product_id FOREIGN KEY (product_id) REFERENCES Product(product_id)
@@ -79,12 +79,12 @@ CREATE TABLE ProductInventory (
 
 CREATE TABLE PaymentTransaction (
     payment_id      INT(10) PRIMARY KEY,
-    payment_method  VARCHAR(50),
-    amount          DECIMAL(10,2),
-    payment_date    DATETIME,
-    is_completed    BOOLEAN,
-    order_id        INT(10),
-    user_id         INT(10),
+    payment_method  VARCHAR(50) NOT NULL,
+    amount          DECIMAL(10,2) NOT NULL,
+    payment_date    DATETIME NOT NULL,
+    is_completed    BOOLEAN NOT NULL,
+    order_id        INT(10) NOT NULL,
+    user_id         INT(10) NOT NULL,
     CONSTRAINT fk_paymenttransaction_order_id FOREIGN KEY (order_id) REFERENCES CustomerOrder(order_id),
     CONSTRAINT fk_paymenttransaction_user_id FOREIGN KEY (user_id) REFERENCES UserAccount(user_id)
 );
@@ -92,8 +92,8 @@ CREATE TABLE PaymentTransaction (
 CREATE TABLE PromotionalOffer (
     promo_code    VARCHAR(25) PRIMARY KEY,
     description   VARCHAR(500),
-    discount      DECIMAL(10,2),
-    expiration    DATE,
-    product_id    INT(10),
+    discount      DECIMAL(10,2) NOT NULL,
+    expiration    DATE NOT NULL,
+    product_id    INT(10) NOT NULL,
     CONSTRAINT fk_promotionaloffer_product_id FOREIGN KEY (product_id) REFERENCES Product(product_id)
 );
