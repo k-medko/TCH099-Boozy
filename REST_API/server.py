762aa0cf6100fe32624595e057f17e1d99b3406a
Tristan Larue <tristanlarue@hotmail.com>
2025-04-01 18:22:24 -0400

    please duyde

--- REST_API/server.py
+++ REST_API/server.py
@@ -31,7 +31,7 @@ def home():
 # HOME
 @app.route('/test')
 def test():
-    return jsonify("Yes, it updated TWICE, LETS FUCKING GOOOOOO, THREE TIMES WTF")
+    return jsonify("Yes, it updated TWICE, LETS FUCKING GOOOOOO, THREE TIMES WTF, FOUR TIMES OMFG!")
 
 # STORES
 @app.route('/getStores', methods=['GET'])
